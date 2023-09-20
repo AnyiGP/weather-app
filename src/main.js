0c4f94acaadde92e7c4db4cd6c0ca275aed08365
AnyiGP <angelesgp31@hotmail.com>
2023-09-20 18:43:13 -0300

    Agrego "" para evitar que se acumulen datos cuando imprimo

--- src/main.js
+++ src/main.js
@@ -1,12 +1,20 @@
 console.log("Hola");
 
+const api = {
+  url: "https://api.openweathermap.org/data/2.5/weather?q=",
+  key: "4e5536b1e0f31785b374595576ddf91c",
+  metricEndPoint: "&units=metric",
+  lang: "&lang=es",
+  iconoURL: "https://openweathermap.org/img/wn/",
+  pais: "AR",
+  
+};
+
 const key = "4e5536b1e0f31785b374595576ddf91c";
 
 const metricEndPoint = "&units=metric";
 
 const lang = "&lang=es";
-
-const inputBuscador = document.getElementById("buscador");
 
 const url = "https://api.openweathermap.org/data/2.5/weather?q=";
 
@@ -16,63 +24,49 @@ const pais = "AR"; //
 
 //const urlPrueba = `https://api.openweathermap.org/data/2.5/weather?q=${provincias},${pais}&appid=${key}${metricEndPoint}${lang}`;
 
-const obtenerUrl = (provincias, pais) => {
-  const urlPrueba = `https://api.openweathermap.org/data/2.5/weather?q=${provincias},${pais}&appid=${key}${metricEndPoint}${lang}`;
-  console.log(urlPrueba);
-  getCity(urlPrueba);
+const selectProvincias = (provincias) => {
+  const urlProvincias = `${api.url}${provincias},${api.pais}&appid=${api.key}${api.metricEndPoint}${api.lang}`;
+  //  console.log(urlPrueba);
+  getCity(urlProvincias);
 };
 
-//const provincias = document.getElementById("Catamarca").value;
-
-const selectElement = document.getElementById("miSelect");
-
-selectElement.addEventListener("change", (e) => {
+//
+document.getElementById("miSelect").addEventListener("change", (e) => {
+  e.preventDefault();
   const provincias = e.target.value;
-  obtenerUrl(provincias, pais);
+  selectProvincias(provincias);
 });
-//hacer que cuando seleccionno una provinica desaparezca la otra event prevent default
-
-const getCity = async (url) => {
-  const res = await fetch(url);
-  const data = await res.json();
-  //const city = data.result
-  console.log(data.weather);
-  console.log(data.name);
-  console.log(data.main.temp);
-  console.log(data.sys.country);
-  //return data.weather.icon
-  let icono = data.weather[0].icon;
-  let row = document.getElementById("divRow");
-  return (row.innerHTML += `<img src="${iconoURL}${icono}@4x.png" alt="">
-  <p class="pTitle">${data.name}</p>
-  <p class="pTitle">${data.main.temp}</p>
-  <p class="pTitle">${data.sys.country}</p>
-  <p class="pTitle">${data.weather[0].description}</p>`);
-};
-
-getCity();
-
-//se puede hacer que recorra todo y que me devuelva solo las ciudades que contengan AR?
-//filtrar toda la data y que me devuelva solo ciudades que tengan ar
-const soloAR = (nombres, search) => {
-  const ciudadesFiltradas = data.filter((nombre) => nombre.includes("AR"));
-  console.log(ciudadesFiltradas);
-};
+//hacer que cuando selecciono una provinica desaparezca la otra
 
 const buscarCiudad = (ciudad) => {
-  const urlCiudad = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${key}${metricEndPoint}${lang}`;
+  const urlCiudad = `${api.url}${ciudad}&appid=${api.key}${api.metricEndPoint}${api.lang}`;
   console.log(urlCiudad);
-  getCity(urlCiudad)
+  getCity(urlCiudad);
 };
 
-// document.querySelector("#search-input").addEventListener("blur", (e) => {
-//   const ciudadDelMundo = e.target.value;
-//   buscarCiudad(ciudadDelMundo);
-// });
-
-/////////////////////////////
 document.querySelector("#search-form").addEventListener("submit", (e) => {
- e.preventDefault()
-  buscarCiudad(document.querySelector("#search-input").value);
+  e.preventDefault();
+  const ciudad = document.querySelector("#search-input").value;
+  buscarCiudad(ciudad);
 });
-
+  
+const getCity = async (url) => {
+    const res = await fetch(url);
+    const data = await res.json();
+    //const city = data.result
+    console.log(data.weather);
+    console.log(data.name);
+    console.log(data.main.temp);
+    console.log(data.sys.country);
+    //return data.weather.icon
+    let icono = data.weather[0].icon;
+    let row = document.getElementById("divRow");
+    row.innerHTML = ""
+    return (row.innerHTML += `<img src="${api.iconoURL}${icono}@4x.png" alt="">
+    <p class="pTitle">${data.name}</p>
+    <p class="pTitle">${data.main.temp}</p>
+    <p class="pTitle">${data.sys.country}</p>
+    <p class="pTitle">${data.weather[0].description}</p>`);
+  };
+  
+  getCity();
