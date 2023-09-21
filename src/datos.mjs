3233300f7aaf54e9367de48d185532c6c23b6530
AnyiGP <angelesgp31@hotmail.com>
2023-09-20 22:54:36 -0300

    creo datos.mjs para llevarme los objetos y luego compartirlos con el main.js

---
+++ src/datos.mjs
@@ -0,0 +1,76 @@
+export const api = {
+    url: "https://api.openweathermap.org/data/2.5/weather?q=",
+    key: "4e5536b1e0f31785b374595576ddf91c",
+    metricEndPoint: "&units=metric",
+    lang: "&lang=es",
+    iconoURL: "https://openweathermap.org/img/wn/",
+    pais: "AR",
+  };
+  
+
+export const provinciasArgentinaArray = [
+    { value: "Ciudad Autónoma de Buenos Aires", label: "Buenos Aires" },
+    { value: "catamarca", label: "Catamarca" },
+    { value: "chaco", label: "Chaco" },
+    { value: "chubut", label: "Chubut" },
+    { value: "Córdoba", label: "Córdoba" },
+    { value: "corrientes", label: "Corrientes" },
+    { value: "Entre Rios", label: "Entre Ríos" },
+    { value: "formosa", label: "Formosa" },
+    { value: "San Salvador de Jujuy", label: "Jujuy" },
+    { value: "La Pampa", label: "La Pampa" },
+    { value: "La Rioja", label: "La Rioja" },
+    { value: "Mendoza", label: "Mendoza" },
+    { value: "misiones", label: "Misiones" },
+    { value: "neuquén", label: "Neuquén" },
+    { value: "rioNegro", label: "Río Negro" },
+    { value: "salta", label: "Salta" },
+    { value: "San Juan", label: "San Juan" },
+    { value: "san Luis", label: "San Luis" },
+    { value: "Santa Cruz Province", label: "Santa Cruz" },
+    { value: "Santa Fe", label: "Santa Fe" },
+    { value: "Santa Fe", label: "Santiago del Estero" },
+    { value: "tierraDelFuego", label: "Tierra del Fuego" },
+    { value: "Tucumán Province", label: "Tucumán" },
+  ];
+
+  // const provinciasArgentina = {
+//   BuenosAires: {
+//     value: "Ciudad Autónoma de Buenos Aires",
+//     label: "Buenos Aires",
+//   },
+//   Catamarca: { value: "catamarca", label: "Catamarca" },
+//   chaco: { value: "chaco", label: "Chaco" },
+//   chubut: { value: "chubut", label: "Chubut" },
+//   cordoba: { value: "Córdoba", label: "Córdoba" },
+//   corrientes: { value: "corrientes", label: "Corrientes" },
+//   entreRios: { value: "Entre Ríos", label: "Entre Ríos" },
+//   formosa: { value: "formosa", label: "Formosa" },
+//   Jujuy: { value: "San Salvador de Jujuy", label: "Jujuy" },
+//   laPampa: { value: "Entre Ríos", label: "La Pampa" },
+//   laRioja: { value: "La Rioja", label: "La Rioja" },
+//   Mendoza: { value: "Mendoza", label: "Mendoza" },
+//   misiones: { value: "misiones", label: "Misiones" },
+//   neuquen: { value: "neuquén", label: "Neuquén" },
+//   rioNegro: { value: "rioNegro", label: "Río Negro" },
+//   salta: { value: "salta", label: "Salta" },
+//   sanJuan: { value: "San Juan", label: "San Juan" },
+//   sanLuis: { value: "san Luis", label: "San Luis" },
+//   santaCruz: { value: "Santa Cruz Province", label: "Santa Cruz" },
+//   santaFe: { value: "Santa Fe", label: "Santa Fe" },
+//   santiagoDelEstero: { value: "Santa Fe", label: "Santiago del Estero" },
+//   tierraDelFuego: { value: "tierraDelFuego", label: "Tierra del Fuego" },
+//   tucuman: { value: "Tucumán Province", label: "Tucumán" },
+// };
+//PARA TRANSFORMAR UN OBJ EN UN ARR DE CLAVES Object.keys(OBJETO).forEach.....
+
+// o usar un for in
+// for (const key in provinciasArgentina) {
+//   if (provinciasArgentina.hasOwnProperty(key)) {
+//     const opcion = document.createElement("option");
+//     opcion.value = provinciasArgentina[key].value;
+//     opcion.textContent = provinciasArgentina[key].label;
+//     miSelect.appendChild(opcion);
+//   }
+// }
+
--- src/main.js
+++ src/main.js
@@ -1,72 +1,5 @@
-console.log("Hola");
-
-////////OBJETOS////////
-
-const api = {
-  url: "https://api.openweathermap.org/data/2.5/weather?q=",
-  key: "4e5536b1e0f31785b374595576ddf91c",
-  metricEndPoint: "&units=metric",
-  lang: "&lang=es",
-  iconoURL: "https://openweathermap.org/img/wn/",
-  pais: "AR",
-};
-
-// const provinciasArgentina = {
-//   BuenosAires: {
-//     value: "Ciudad Autónoma de Buenos Aires",
-//     label: "Buenos Aires",
-//   },
-//   Catamarca: { value: "catamarca", label: "Catamarca" },
-//   chaco: { value: "chaco", label: "Chaco" },
-//   chubut: { value: "chubut", label: "Chubut" },
-//   cordoba: { value: "Córdoba", label: "Córdoba" },
-//   corrientes: { value: "corrientes", label: "Corrientes" },
-//   entreRios: { value: "Entre Ríos", label: "Entre Ríos" },
-//   formosa: { value: "formosa", label: "Formosa" },
-//   Jujuy: { value: "San Salvador de Jujuy", label: "Jujuy" },
-//   laPampa: { value: "Entre Ríos", label: "La Pampa" },
-//   laRioja: { value: "La Rioja", label: "La Rioja" },
-//   Mendoza: { value: "Mendoza", label: "Mendoza" },
-//   misiones: { value: "misiones", label: "Misiones" },
-//   neuquen: { value: "neuquén", label: "Neuquén" },
-//   rioNegro: { value: "rioNegro", label: "Río Negro" },
-//   salta: { value: "salta", label: "Salta" },
-//   sanJuan: { value: "San Juan", label: "San Juan" },
-//   sanLuis: { value: "san Luis", label: "San Luis" },
-//   santaCruz: { value: "Santa Cruz Province", label: "Santa Cruz" },
-//   santaFe: { value: "Santa Fe", label: "Santa Fe" },
-//   santiagoDelEstero: { value: "Santa Fe", label: "Santiago del Estero" },
-//   tierraDelFuego: { value: "tierraDelFuego", label: "Tierra del Fuego" },
-//   tucuman: { value: "Tucumán Province", label: "Tucumán" },
-// };
-
-////////ARRAY OBJETOS////////
-
-const provinciasArgentinaArray = [
-  { value: "Ciudad Autónoma de Buenos Aires", label: "Buenos Aires" },
-  { value: "catamarca", label: "Catamarca" },
-  { value: "chaco", label: "Chaco" },
-  { value: "chubut", label: "Chubut" },
-  { value: "Córdoba", label: "Córdoba" },
-  { value: "corrientes", label: "Corrientes" },
-  { value: "Entre Ríos", label: "Entre Ríos" },
-  { value: "formosa", label: "Formosa" },
-  { value: "San Salvador de Jujuy", label: "Jujuy" },
-  { value: "Entre Ríos", label: "La Pampa" },
-  { value: "La Rioja", label: "La Rioja" },
-  { value: "Mendoza", label: "Mendoza" },
-  { value: "misiones", label: "Misiones" },
-  { value: "neuquén", label: "Neuquén" },
-  { value: "rioNegro", label: "Río Negro" },
-  { value: "salta", label: "Salta" },
-  { value: "San Juan", label: "San Juan" },
-  { value: "san Luis", label: "San Luis" },
-  { value: "Santa Cruz Province", label: "Santa Cruz" },
-  { value: "Santa Fe", label: "Santa Fe" },
-  { value: "Santa Fe", label: "Santiago del Estero" },
-  { value: "tierraDelFuego", label: "Tierra del Fuego" },
-  { value: "Tucumán Province", label: "Tucumán" },
-];
+import {api} from './datos.mjs'
+import {provinciasArgentinaArray} from './datos.mjs'
 
 ////////SELECT DE PROVINCIAS////////
 
@@ -78,20 +11,11 @@ const selectProvincias = (provincias) => {
 
 const miSelect = document.getElementById("miSelect");
 
-// for (const key in provinciasArgentina) {
-//   if (provinciasArgentina.hasOwnProperty(key)) {
-//     const opcion = document.createElement("option");
-//     opcion.value = provinciasArgentina[key].value;
-//     opcion.textContent = provinciasArgentina[key].label;
-//     miSelect.appendChild(opcion);
-//   }
-// }
-
 provinciasArgentinaArray.forEach((provincia) => {
   const opcion = document.createElement("option");
-  opcion.value = provincia.value; // Establece el valor de la opción
-  opcion.textContent = provincia.label; // Establece el texto de la opción
-  miSelect.appendChild(opcion); // Agrega la opción al select
+  opcion.value = provincia.value;
+  opcion.textContent = provincia.label;
+  miSelect.appendChild(opcion); 
 });
 
 miSelect.addEventListener("change", (e) => {
